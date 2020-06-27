94aadddb0b0b9483ca8b17b9a49a09c2fbb3bd49
wyozi <joonas.mertanen@gmail.com>
2020-06-28 05:24:09 +0800

    Display each file's filesize in cp

--- src/bin/mdl.rs
+++ src/bin/mdl.rs
@@ -1,6 +1,6 @@
+use std::fs;
 use std::path::{Path, PathBuf};
 use std::vec::Vec;
-use std::fs;
 
 use clap::Clap;
 
@@ -14,17 +14,17 @@ struct Opts {
 #[derive(Clap)]
 enum SubCommand {
     /// Lists all dependencies of this .mdl
-    /// 
+    ///
     /// Namely, this will return a list of paths that
     /// are required for the .mdl to function, including the
     /// .mdl file itself.
-    /// 
+    ///
     /// The paths are returned relative to the working directory
     #[clap(alias = "deps")]
     Dependencies(DependenciesCommand),
 
     /// Copies given .mdl with dependencies to target path
-    /// 
+    ///
     /// Maintains the folder structure, including materials.
     /// For that reason, the target directory should be one
     /// with "models" and "materials" folders
@@ -32,12 +32,12 @@ enum SubCommand {
     Copy(CopyCommand),
 
     /// Removes given .mdl (and optionally its dependencies)
-    /// 
+    ///
     /// By default this command removes just direct dependencies
     /// (the files references the .mdl in the same models folder).
     /// This behavior can be changed with the deps flag
     #[clap(alias = "rm")]
-    Remove(RemoveCommand)
+    Remove(RemoveCommand),
 }
 
 #[derive(Clap)]
@@ -58,7 +58,7 @@ struct CopyCommand {
     /// Prints what the command would copy if executed without
     /// this flag
     #[clap(long, short = "n")]
-    dry_run: bool
+    dry_run: bool,
 }
 
 #[derive(Clap)]
@@ -67,7 +67,7 @@ struct RemoveCommand {
     input: String,
 
     /// Also remove in-direct dependencies.
-    /// 
+    ///
     /// In-direct assets are the ones that other models may rely on,
     /// such as materials and textures.
     #[clap(long, short)]
@@ -76,7 +76,7 @@ struct RemoveCommand {
     /// Prints what the command would remove if executed without
     /// this flag
     #[clap(long, short = "n")]
-    dry_run: bool
+    dry_run: bool,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -92,12 +92,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
 
             Ok(())
-        },
+        }
         SubCommand::Copy(t) => {
             let path = Path::new(&t.input);
             let out_path = Path::new(&t.output_folder);
 
-            let mut copy_map: Vec<(PathBuf, PathBuf)> = vec!();
+            let mut copy_map: Vec<(PathBuf, PathBuf)> = vec![];
 
             let mdl = steamws::mdl::MDLFile::open(path)?;
             let assets_path = mdl.assets_path();
@@ -114,12 +114,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             let mut size = 0;
 
             for (from, to) in copy_map {
-                println!("{} -> {}", from.display(), to.display());
+                let this_size = fs::metadata(&from).ok().map(|m| m.len());
+                println!(
+                    "{} -> {} ({})",
+                    from.display(),
+                    to.display(),
+                    this_size
+                        .map(|s| steamws::human_readable_size(s))
+                        .unwrap_or_else(|| "Unknown".to_string())
+                );
                 if !t.dry_run {
                     fs::create_dir_all(&to.parent().unwrap())?;
                     fs::copy(&from, &to)?;
                 } else {
-                    size += fs::metadata(from).ok().map(|m| m.len()).unwrap_or(0);
+                    size += this_size.unwrap_or(0);
                 }
             }
 
@@ -129,7 +137,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
 
             Ok(())
-        },
+        }
         SubCommand::Remove(t) => {
             let path = Path::new(&t.input);
             let mdl = steamws::mdl::MDLFile::open(path)?;
@@ -161,6 +169,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
 
             Ok(())
-        },
+        }
     }
 }
