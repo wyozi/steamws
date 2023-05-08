43d7afbed3b43150b411e3defb6d63749f140cf0
Joonas <joonas.mertanen@gmail.com>
2023-05-08 15:37:36 +0300

    improve vmt key extraction

--- src/vmt.rs
+++ src/vmt.rs
@@ -1,16 +1,16 @@
+use std::collections::HashSet;
 use std::fs;
 use std::path::Path;
-use std::collections::HashSet;
 
 use lazy_static::lazy_static;
-use regex::RegexBuilder;
 use regex::Regex;
+use regex::RegexBuilder;
 
 use std::iter::FromIterator;
 
 #[derive(Debug)]
 pub struct VMT {
-    pub textures: Vec<String>
+    pub textures: Vec<String>,
 }
 
 pub fn read(path: &Path) -> Result<VMT, Box<dyn std::error::Error>> {
@@ -22,28 +22,26 @@ pub fn read(path: &Path) -> Result<VMT, Box<dyn std::error::Error>> {
         static ref KEYS: HashSet<&'static str> = {
             let mut m = HashSet::new();
             m.insert("basetexture");
-	        m.insert("iris");
-	        m.insert("ambientoccltexture");
-	        m.insert("bumpmap");
-	        m.insert("phongexponenttexture");
-	        m.insert("detail");
-	        m.insert("selfillummask");
+            m.insert("iris");
+            m.insert("ambientoccltexture");
+            m.insert("bumpmap");
+            m.insert("phongexponenttexture");
+            m.insert("detail");
+            m.insert("selfillummask");
+            m.insert("lightwarptexture");
+            m.insert("envmap");
+            m.insert("corneatexture");
             m
         };
     }
     let string = fs::read_to_string(path)?;
 
-    let textures: HashSet<String> = RE.captures_iter(&string)
-        .filter(|c| KEYS.contains(&c["key"]))
-        .map(|c| {
-            c["value"]
-            .trim()
-            .trim_matches('"')
-            .trim()
-            .to_owned()
-        })
+    let textures: HashSet<String> = RE
+        .captures_iter(&string)
+        .filter(|c| KEYS.contains(&c["key"].to_lowercase() as &str))
+        .map(|c| c["value"].trim().trim_matches('"').trim().to_owned())
         .collect();
     Ok(VMT {
-        textures: Vec::from_iter(textures)
+        textures: Vec::from_iter(textures),
     })
 }
